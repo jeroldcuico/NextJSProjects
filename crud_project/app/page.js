fdef4d22a71422ec3c2781031a674313e8921875
jeroldcuico <loverold17@gmail.com>
2025-02-12 12:03:34 +0800

    Fixes on UI Tailwind and texts

--- crud_project/app/page.js
+++ crud_project/app/page.js
@@ -2,9 +2,5 @@ import List from "@/app/components/List";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <>
-      <List />
-    </>
-  );
+  return <List />;
 }
